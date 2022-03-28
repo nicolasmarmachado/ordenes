2dbeec34c932abcd791473f0492e331916db8c0c
nicolasmarmachado <101880825+nicolasmarmachado@users.noreply.github.com>
2022-03-28 19:44:53 -0300

    Creación de NavBar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
 
-function App() {
-  let saludo="Hola Mundo!! Mi primer entregable!";
+export default function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        {saludo}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      <NavBar>
+        
+      </NavBar>
+
+    </>
   );
 }
-
-export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./estilos.css";
+
+
+export default function NavBar() {
+  return (
+    <>  
+
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Hurricane&display=swap');
+      </style>
+        
+      <div className="navbar">        
+        <img src="logo_NI.jpg" alt="logo" className="logo"/>
+        <ul className="items">
+          <li><a href="#">Quienes somos</a></li>
+          <li><a href="#">Tienda</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </div>
+    </>
+  )
+};
